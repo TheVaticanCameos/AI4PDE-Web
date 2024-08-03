4d0143588fdc2e8c1ddbfb79bac5e6663fdda3b3
iw17 <iw17@mail.ustc.edu.cn>
2024-08-04 01:05:11 +0800

    🎨 reuse code in 'infer.py'
    I found function 'poisson1d_solver' in 'server.py' identical to that
    in 'infer.py', so the latter is reused and imported to 'server.py'.
    And the degree `max(poly.keys())` are modified again.
    
    I think it's abstract to put a varying PNG image into the repo.

--- front-end/server.py
+++ front-end/server.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify, send_file
-import deepxde as dde
-import matplotlib.pyplot as plt
-import numpy as np
 import os
+import sys
 from pathlib import Path
+
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
 root_dir: str = str(Path(__file__).parent.parent)
@@ -11,85 +10,14 @@ model_path: str = os.path.join(root_dir, 'back-end', 'params', 'params.ckpt-1000
 png_path: str = os.path.join(root_dir, 'back-end', 'output', 'poisson1d-test.png')
 html_path: str = os.path.join(root_dir, 'front-end', 'PDE-solver2.0.html')
 
-def poisson1d_solver(poly: dict) -> dict[str, list[float]]:
-    # Poisson equation: -u_xx = f
-    def equation(x, y, f):
-        dy_xx = dde.grad.hessian(y, x)
-        return -dy_xx - f
-
-    # Domain is interval [0, 1]
-    geom = dde.geometry.Interval(0, 1)
-
-    # Zero Dirichlet BC
-    def u_boundary(_):
-        return 0
-
-    def boundary(_, on_boundary):
-        return on_boundary
-
-    bc = dde.icbc.DirichletBC(geom, u_boundary, boundary)
-
-    # Define PDE
-    pde = dde.data.PDE(geom, equation, bc, num_domain=100, num_boundary=2)
-
-    # Function space for f(x) are polynomials
-    degree = max(poly.keys())
-    space = dde.data.PowerSeries(N=degree + 1)
-
-    # Choose evaluation points
-    num_eval_points = 10
-    evaluation_points = geom.uniform_points(num_eval_points, boundary=True)
-
-    # Define PDE operator
-    pde_op = dde.data.PDEOperatorCartesianProd(
-        pde,
-        space,
-        evaluation_points,
-        num_function=100,
-    )
-
-    # Setup DeepONet
-    dim_x = 1
-    p = 32
-    net = dde.nn.DeepONetCartesianProd(
-        [num_eval_points, 32, p],
-        [dim_x, 32, p],
-        activation="tanh",
-        kernel_initializer="Glorot normal",
-    )
-
-    # Define and train model
-    model = dde.Model(pde_op, net)
-    dde.optimizers.set_LBFGS_options(maxiter=1000)
-    model.compile("L-BFGS")
-    model.restore(model_path, device='cpu')
-
-    max_deg = max(poly.keys())
-    features = np.zeros(shape=(1, max_deg+1), dtype=np.float32)
-    for key, value in poly.items():
-        features[0][key] = value
-
-    fx = space.eval_batch(features, evaluation_points)
-    x = geom.uniform_points(100, boundary=True)
-    y = model.predict((fx, x))
-
-    fig = plt.figure(figsize=(4, 8))
-    z = np.zeros_like(x)
-    plt.plot(x, z, 'k-', alpha=0.1)
-    plt.plot(evaluation_points, np.transpose(fx), '--', label=r'$f(x)$')
-    plt.plot(x, np.transpose(y), '-', label=r'$u(x)$')
-    plt.legend()
-    plt.title("Solution of 1d Poisson equation")
-    plt.savefig(png_path)
-    plt.close()
-    return {'x': x.ravel().tolist(), 'y': y.ravel().tolist()}
+sys.path.append(os.path.join(root_dir, 'back-end', 'source'))
+from infer import poisson1d_solver # type: ignore
 
 @app.route('/solve', methods=['POST'])
 def solve():
     data = request.json
     poly = {item['power']: item['value'] for item in data}
     return poisson1d_solver(poly)
-    #return send_file('../output/poisson1d-test.png', mimetype='image/png')
 
 @app.route('/')
 def main_page():
